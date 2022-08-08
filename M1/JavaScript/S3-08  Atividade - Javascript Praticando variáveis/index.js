0fc02f0aea0de90be155a9689808ce1b23d7e4fe
Jessica Ferreira Pereira <jessicafpereira16@gmail.com>
2022-08-08 15:29:04 -0300

    exercicios de segunda e terça de js

---
+++ M1/JavaScript/S3-08  Atividade - Javascript Praticando variáveis/index.js
@@ -0,0 +1,66 @@
+//exercicio 1
+// let nome = "Jéssica"
+// console.log (`Olá, eu sou a/o ${nome}`)
+
+//exercicio 2
+
+// let nome = "Jéssica"
+// let sobrenome = "Estevam"
+// console.log(`Olá, eu me chamo ${nome} ${sobrenome} `);
+
+//exercicio 3
+
+// let primeiroNome = "Jéssica"
+// let segundoNome = "Pereira"
+// let idade = 29
+// console.log(`Olá, meu primeiro nome é ${primeiroNome}, meu segundo nome é ${segundoNome}. E eu tenho ${idade} anos.`);
+
+
+//exercicio 4
+// let preco1 = 2
+// let preco2 = 3
+// let totalGasto = preco1 + preco2
+// console.log(`Comprei uma xícara por ${preco1} reais, e dois copos por ${preco2} reais. No total, gastei ${totalGasto}.`);
+
+//exercicio 6
+// let contador = 0
+// console.log(contador);
+// contador = 1
+// console.log(contador);
+// contador = contador +1
+// console.log(contador);
+// contador = contador+1
+// console.log(contador);
+// contador= contador+1
+// console.log(contador);
+// contador = contador +1
+// console.log(contador)
+
+//exercicio 7
+// let anoAtual = 2022
+// let anoNascimento = 1992
+// let idade = anoAtual - anoNascimento
+// console.log(`No ano ${anoAtual}, eu tenho/vou ter ${idade} anos de idade`);
+
+//exercicio 8
+
+// let alturaRetangulo = 5
+// let larguraRetangulo = 7
+// let areaRetangulo = alturaRetangulo * larguraRetangulo
+// console.log(`Um retângulo com largura de ${larguraRetangulo} e altura de ${alturaRetangulo}, possui ${areaRetangulo}cm² de área total.`);
+
+
+//exercicio 9
+// let diaAtual = "08/"
+// let mesAtual = "08/"
+// let anoAtual = "2022"
+// let dataAtual = diaAtual+mesAtual+anoAtual
+// console.log(`A data de hoje é ${dataAtual}`);
+
+//exercicio 10
+// let valorA = 10
+// let valorB = 12
+// let novoA= valorB
+// let novoB = valorA
+// console.log(`Antes a variável valorA possuía o valor ${valorA}, agora possui o valor ${novoA}.`);
+// console.log(`Antes a variável valorB possuía o valor ${valorB}, agora possui o valor ${novoB}.`);
---
+++ M1/JavaScript/S3-10   Atividade - Javascript Trabalhando com Strings/index.js
@@ -0,0 +1,20 @@
+//exercicio 1
+
+// let meuNome = 'Jéssica'
+// let comprimentoNome = meuNome.length
+// console.log(`O meu nome possui ${comprimentoNome} caracteres de comprimento.`);
+
+// //exercicio 2
+
+// let preco = 15
+// let precoFormatado = 'R$'+preco
+// console.log(`O valor ${preco} formatado ficaria assim: ${precoFormatado}`);
+
+//exercicio 3
+
+// let cumprimento = 'Bom dia'
+// let nome= 'Joana'
+// let sobrenome= 'Aguiar'
+// let mensagemCumprimento = `${cumprimento}, ${nome} ${sobrenome}`
+// console.log(mensagemCumprimento);
+
