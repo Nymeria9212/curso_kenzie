70cb8c259ea9f4ab09566e2e92c1b7bed2267039
Jessica Ferreira Pereira <jessicafpereira16@gmail.com>
2022-08-10 13:49:59 -0300

    Javascript Comparações atividades

---
+++ M1/JavaScript/S3-16  Atividade - Javascript Comparações/script.js
@@ -0,0 +1,21 @@
+// Exercício 1
+
+// let valor1= 10
+// let valor2 ='10'
+// let comparaçãoDeValores = valor1 === valor2
+
+// console.log (comparaçãoDeValores)
+
+// Exercício 2
+
+// let usuario1Altura = 1.80
+// let usuario2Altura = 1.75
+// let comparacaooDeAlturas = usuario1Altura>usuario2Altura
+// console.log(comparacaooDeAlturas);
+
+// Exercício 3
+
+// let valor1 = 10
+// let valor2 = '10'
+// let comparacaooDeValores = valor1 !== valor2
+// console.log(comparacaooDeValores);
