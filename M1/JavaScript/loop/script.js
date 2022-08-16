edd822dd65641ce3a473a84498b987272f1961af
Jessica Ferreira Pereira <jessicafpereira16@gmail.com>
2022-08-16 18:02:06 -0300

    exemplos de loop

---
+++ M1/JavaScript/loop/script.js
@@ -0,0 +1,54 @@
+//exemplo loop
+
+//while
+
+// const fruits = ['banana', 'maça', 'abacate']
+// let i = 0
+
+// while (i < fruits.length) {
+//     console.log(fruits[i]) //valorconsole.log(i) //índice
+//     i = i + 1
+// }
+
+
+//do while
+
+// const list = [ 'a' , 'b' , 'c' ]
+// let i = 0
+// do {
+//     console.log(list[i]) //valorconsole.log(i) //índice
+//     i = i + 1
+// } while (i < list.length)
+
+
+//for
+// Exibir os números de 1 à 5
+// const n = 5;
+// // looping de i = 1 até 5// em cada iteração, i é aumentado em 1
+// for (let i = 1; i <= n; i++) {
+//     console.log(i); // imprimindo o valor de i
+// }
+
+
+//exemplo 2 for
+
+
+// let states = [
+//     'Alabama',
+//     'Alaska',
+//     'Arizona',
+//     'Arkansas',
+//     'California',
+//     'Colorado',
+//     'Connecticut',
+//     'Delaware',
+//     'Florida',
+//     'Georgia'
+// ]
+// for (let i = 0; i < states.length; i++) {
+//     console.log(states[i])
+// }
+
+// for (let i = 0; i < states.length; i += 2) {
+//     console.log(states[i])
+// }
