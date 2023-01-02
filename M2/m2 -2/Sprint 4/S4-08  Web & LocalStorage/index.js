2243dab40247879d036f154836c0889964685695
Jessica Ferreira Pereira <jessicafpereira16@gmail.com>
2023-01-02 19:21:43 -0300

    atividade

---
+++ M2/m2 -2/Sprint 4/S4-08  Web & LocalStorage/index.js
@@ -0,0 +1,16 @@
+//criando variáveis
+
+const numero = 25;
+
+const array = [1, 3, true, "M2"];
+
+const string = "JavaScript";
+
+
+//Adicionando os valores no local storage
+
+localStorage.setItem("Número", numero);
+
+localStorage.setItem("Um array", array);
+
+localStorage.setItem("String qualquer", string);
